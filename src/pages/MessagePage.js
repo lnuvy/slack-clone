16ec93e7c1e3d9491923470acc6b5229ca5b159a
Deokhaeng <101077014+Deokhaeng@users.noreply.github.com>
2022-04-20 17:51:41 +0900

    socket.io 기반 채팅 뷰 구현 및 연결

--- src/pages/MessagePage.js
+++ src/pages/MessagePage.js
@@ -4,13 +4,25 @@ import Massage from "../components/massage/Massage";
 import MassageBox from "../components/massage/MassageBox";
 import MassageHeader from "../components/massage/MassageHeader";
 
+//리덕스
+import { useSelector } from "react-redux";
+
+//소켓
+import io from "socket.io-client";
+
+export const socket = io.connect("ws://52.78.246.163:80");
+socket.emit("init", { name: "testName" });
+
 const MessagePage = () => {
+  const userNickname = useSelector((state) => state.user.user.nickname);
+  console.log(userNickname);
+
   return (
     <>
       <MessagePageWrap>
         <MassageHeader />
         <Massage />
-        <MassageBox />
+        <MassageBox userNickname={userNickname} />
       </MessagePageWrap>
     </>
   );
@@ -18,7 +30,7 @@ const MessagePage = () => {
 
 const MessagePageWrap = styled.div`
   width: 100%;
-  height: calc(100vh - 10px);
+  height: calc(100vh - 44px);
   display: flex;
   flex-direction: column;
 `;
